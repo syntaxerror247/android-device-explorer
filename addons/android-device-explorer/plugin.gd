e07fca1a5d0d08d244b9968ef767d0c7a6030e52
Anish Kumar <mishragames@gmail.com>
2026-04-09 13:11:15 +0530

    Rename plugin directory

--- addons/android-device-explorer/plugin.gd
+++ addons/android-device-explorer/plugin.gd
@@ -4,13 +4,13 @@ extends EditorPlugin
 var dock
 
 func _enter_tree():
-	var dock_control = preload("res://addons/android_device_explorer/device_explorer.gd").new()
+	var dock_control = preload("res://addons/android-device-explorer/device_explorer.gd").new()
 	dock = EditorDock.new()
 	dock.add_child(dock_control)
 	dock.title = "Device Explorer"
 	dock.default_slot = EditorDock.DOCK_SLOT_RIGHT_BL
 	dock.available_layouts = EditorDock.DOCK_LAYOUT_VERTICAL | EditorDock.DOCK_LAYOUT_FLOATING
-	dock.dock_icon = load("res://addons/android_device_explorer/device_explorer_icon.svg")
+	dock.dock_icon = load("res://addons/android-device-explorer/device_explorer_icon.svg")
 	add_dock(dock)
 
 func _exit_tree():
